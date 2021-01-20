c6837c910e21c3635fd9b55819813932a602ecaf
Muhittin AYRI <ayri.muhittin@outlook.com>
2021-01-20 23:53:15 +0300

    Add project files.

---
+++ ProductManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+        }
+
+        //void ? = işlemini yapar ve bitirir.(emir kipi) 
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.ProductName = "Masa";
+            product1.CategoryId = 2;
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+
+            Product product2 = new Product
+            {
+                Id = 2,
+                CategoryId = 5,
+                UnitsInStock = 5,
+                ProductName = "Kalem",
+                UnitPrice = 35
+            };
+
+
+            //case sensitive - Küçük büyük harf duyarlılığı
+            //İnstance - Örnek oluşturma
+            //PascalCase   //camelCase
+            //Stack de oluşuur                  //Heap de oluşur
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+            
+
+            //int,double,bool - değer tip
+            //diziler,class,abstract class, interface - referans tip
+            //ref out
+
+        }
+    }
+}
